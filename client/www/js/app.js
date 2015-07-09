1dd03bd35bba15dfe525c1bde0ad66de5069e8cb
Michael Kroll <michael.kroll.94@googlemail.com>
2015-07-09 14:42:50 +0200

    Unnötige resize Methode entfernt

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -22,28 +22,6 @@ define([
             console.log(this.game);
         };
 
-        App.prototype.onResize = function(){
-            var self = this;
-            if(this.canvas !== null){
-
-                this.width = window.innerWidth * window.devicePixelRatio;
-                this.height = window.innerHeight * window.devicePixelRatio;
-
-                this.game.renderer.resize(this.width,this.height);
-                this.game.width = this.width;
-                this.game.height = this.height;
-                this.game.world.setBounds(0,0,this.width,this.height);
-
-                setTimeout(function(){
-                    self.canvas.style.width = window.innerWidth + 'px !important';
-                    self.canvas.style.height = window.innerHeight + 'px !important';
-                },100);
-
-            }else{
-                alert('oi canvas = null!');
-            }
-        };
-
         return new App();
 
 });
