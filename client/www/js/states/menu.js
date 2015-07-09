7a22ca4ac1ebdc0999b626ccece38c4fbbada680
Jan Friesen <jan-friesen@web.de>
2015-07-09 16:40:19 +0200

    logo als platzhalter hinzugefügt

--- client/www/js/states/menu.js
+++ client/www/js/states/menu.js
@@ -12,20 +12,24 @@ define(function(){
     Menu.prototype = new Phaser.State();
 
     Menu.prototype.init = function(){
-        this.app = require("app");
+        this.app = require('app');
     };
 
     Menu.prototype.preload = function(){
-        this.app.load('image',"background", "mainmenu/background.png");
+        this.app.load('image','background', 'mainmenu/background.png');
+        this.app.load('image', 'logo', 'mainmenu/logo.png');
 
     };
 
     Menu.prototype.create = function(){
-        this.background = this.add.sprite(0,0,"background");
+        this.background = this.add.sprite(0,0,'background');
 
         var heightRatio = this.app.height/this.background.height;
         this.background.height = this.app.height;
         this.background.width = this.background.width * heightRatio;
+
+        this.logo = this.add.sprite(this.world.centerX, this.world.centerY - (this.app.height / 3), 'logo');
+        this.logo.anchor.set(0.5);
     };
 
 
