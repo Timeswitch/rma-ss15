03bea0585a4e13c537546267e425a36fe4d5645a
Michael Kroll <m.kroll@thamm-it.de>
2015-07-24 11:15:50 +0200

    Beispiel Server

--- server/app/index.js
+++ server/app/index.js
@@ -1,3 +1,21 @@
 /**
  * Created by michael on 24/07/15.
  */
+
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.end('Nothing to see');
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection',function(socket){
+    socket.emit('welcome',{
+        message: 'Welcome!',
+        id: socket.id
+    })
+});
+
+server.listen(2209);
