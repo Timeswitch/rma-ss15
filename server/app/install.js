846e2a19f79e4e72b273d8222277d7a815329a9b
Michael Kroll <michael.kroll.94@googlemail.com>
2015-08-09 23:28:40 +0200

    Grundlage zum Anlegen der Standard Items

--- server/app/install.js
+++ server/app/install.js
@@ -2,8 +2,30 @@
  * Created by michael on 26/07/15.
  */
 
+var Promise = require('bluebird');
 var database = require('./Database.js');
+var RobotPart = require('./Models/RobotPart.js');
 
 database.knex.migrate.latest().then(function(){
-    process.exit();
+
+    var defaultItems = [];
+
+    function newItem(name, slot, image, agility, attack, defense, effect){
+        defaultItems.push(RobotPart.forge({
+            name: name,
+            slot: slot,
+            image: image,
+            agility: agility,
+            attack: attack,
+            defense: defense,
+            effect: effect
+        }).save());
+    }
+
+    //Heads
+    newItem('test','head',0,0,1,1,null);
+
+    Promise.all(defaultItems).then(function(){
+        process.exit();
+    });
 });
