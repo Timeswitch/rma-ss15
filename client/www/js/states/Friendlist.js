4a263465f2bb851c268d780e49aa1b81a9f11085
Michael Kroll <michael.kroll.94@googlemail.com>
2015-09-01 12:35:24 +0200

    Eingabefeld für Freundesliste

--- client/www/js/states/Friendlist.js
+++ client/www/js/states/Friendlist.js
@@ -13,8 +13,29 @@ define([
     Friendlist.prototype = Object.create(BaseState.prototype);
     Friendlist.prototype.constructor = Friendlist;
 
-    Friendlist.prototype.create = function(){
+    Friendlist.prototype.init = function(){
+        this.app = require('app');
+        this.friendInput = null;
+    };
 
+    Friendlist.prototype.create = function(){
+        this.friendInput = document.createElement('input');
+        this.friendInput.type = 'text';
+        this.friendInput.style.width = (this.app.width - 84) + 'px';
+        this.friendInput.style.height = 48 + 'px';
+        this.friendInput.style.position = 'absolute';
+        this.friendInput.style.bottom = '0px';
+        this.friendInput.style.fontSize = '24px';
+        this.friendInput.style.fontFamily = 'vt323regular';
+        this.friendInput.style.color = '#ffffff';
+        this.friendInput.style.backgroundColor = '#419001';
+        this.friendInput.style.borderColor = '#1F4005';
+        document.body.appendChild(this.friendInput);
+    };
+
+    Friendlist.prototype.shutdown = function(){
+        this.friendInput.parentNode.removeChild(this.friendInput);
+        this.friendInput = null;
     };
 
     return new Friendlist();
