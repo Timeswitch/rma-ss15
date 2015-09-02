fe85492c41c61464660680f54a3cef82f7424d6c
Michael Kroll <michael.kroll.94@googlemail.com>
2015-09-02 02:07:37 +0200

    Titelleiste

--- client/www/js/states/Friendlist.js
+++ client/www/js/states/Friendlist.js
@@ -3,8 +3,9 @@
  */
 
 define([
-    'states/BaseState'
-],function(BaseState){
+    'states/BaseState',
+    'gameobjects/TileBox'
+],function(BaseState,TileBox){
 
     function Friendlist(){
         this.app = null;
@@ -17,13 +18,47 @@ define([
         this.app = require('app');
         this.friendInput = null;
         this.friendButton = null;
+        this.titleContainer = null;
     };
 
     Friendlist.prototype.preload = function(){
         this.load.spritesheet('buttonPlus', 'assets/spritesheets/plus.png',60,60);
+
+        this.load.image('boxTL','assets/tiles/alert_tl.png');
+        this.load.image('boxT','assets/tiles/alert_t.png');
+        this.load.image('boxTR','assets/tiles/alert_tr.png');
+        this.load.image('boxL','assets/tiles/alert_l.png');
+        this.load.image('boxM','assets/tiles/alert_m.png');
+        this.load.image('boxR','assets/tiles/alert_r.png');
+        this.load.image('boxBL','assets/tiles/alert_bl.png');
+        this.load.image('boxB','assets/tiles/alert_b.png');
+        this.load.image('boxBR','assets/tiles/alert_br.png');
     };
 
     Friendlist.prototype.create = function(){
+
+        this.titleContainer = new TileBox(this.app.game,{
+            topLeft: 'boxTL',
+            topRight: 'boxTR',
+            top: 'boxT',
+            left: 'boxL',
+            center: 'boxM',
+            right: 'boxR',
+            bottomLeft: 'boxBL',
+            bottomRight: 'boxBR',
+            bottom: 'boxB'
+        },30,this.app.width-60,0);
+
+        this.titleContainer.x = 0;
+        this.titleContainer.y = 0;
+
+        var titleText = this.app.game.add.text(this.world.centerX,30,'Freunde',{font: "35px bitwise",fill: '#ffffff',align: 'center'});
+        titleText.anchor.set(0.5);
+        this.titleContainer.add(titleText);
+        this.titleContainer.forEach(function(e){
+            e.alpha = 0.7;
+        },this);
+
         this.friendInput = document.createElement('input');
         this.friendInput.type = 'text';
         this.friendInput.style.width = (this.app.width - 66) + 'px';
